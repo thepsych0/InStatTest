80b4c11ff04a9aa06ccc25cd5ecdc494d921ea0e
Edgar <edserobian@yandex.ru>
2019-01-19 21:06:02 +0300

    Initial Commit

---
+++ InStat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InStat
+//
+//  Created by ThePsych0 on 19/01/2019.
+//  Copyright © 2019 Edgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
